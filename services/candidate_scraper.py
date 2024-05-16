6cef51e6351333c960e9d05a42530b41982ec627
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-05-17 00:45:46 +0530

    Update candidate_scraper.py

--- services/candidate_scraper.py
+++ services/candidate_scraper.py
@@ -4,6 +4,7 @@ from services.scraping_utils import (
     options, service, search_for_candidate_name, search_for_candidate_headline, 
     search_for_section, search_for_candidate_profile_picture, add_session_cookie
 )
+from pyvirtualdisplay import Display
 from concurrent.futures import ThreadPoolExecutor
 import json
 from multiprocessing import Pool
@@ -21,35 +22,39 @@ def scrape_linkedin_profile(linkedin_ids):
     
 def scrape_profile_worker(linkedin_id):
     try:
+        display = Display(visible=0, size=(1920, 1080))
+        display.start()
+
         # Setup Selenium WebDriver
         driver = webdriver.Chrome(service=service, options=options)
-        
+
         # Load cookies from the file
         add_session_cookie(driver)
-        
+
         print(f'Scraping data for id: {linkedin_id}')
-        
+
         # LinkedIn URL for the profile
         profile_url = f"https://www.linkedin.com/in/{linkedin_id}/"
-        
+
         # Navigate to the LinkedIn profile
         driver.get(profile_url)
-        
+
         if "/404" in driver.current_url or "Page not found" in driver.page_source:
             driver.quit()
+            display.stop()
             print(f"Profile for {linkedin_id} not found (404)")
             return {"error": f"Profile for {linkedin_id} not found."}
-        
+
         sleep(1)
-        
+
         # Scrape name, headline, education, experience, and profile picture URL from the LinkedIn profile
         try:
             name = search_for_candidate_name(driver)
             if not name:
                 driver.quit()
+                display.stop()
                 print("scraping failed due to session token not setup or expired")
                 return {"error": "Your Linkedin session token is not set up correctly or has expired"}
-            
             headline = search_for_candidate_headline(driver)
             education = search_for_section(driver, "Education")
             experience = search_for_section(driver, "Experience")
@@ -57,11 +62,12 @@ def scrape_profile_worker(linkedin_id):
         except Exception as e:
             print(f"Error scraping details for {linkedin_id} : {e}")
             driver.quit()
+            display.stop()
             return {"error": f"Error searching for details for {linkedin_id}"}
-        
+
         driver.quit()
+        display.stop()
         print(f"Finished fetching details for profile {linkedin_id} successfully")
-        
         return {
             "linkedin_id": linkedin_id,
             "name": name,
