5a0ba8e5a1100cbf173475964b3b0f0388d6f286
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-05-16 12:19:44 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 import uvicorn
-from api.routes import app
+from fastapi import FastAPI
+from starlette.requests import Request
+import time
+
+app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    print(f"Handled request in {process_time} seconds")
+    return response
 
 if __name__ == '__main__':
-    uvicorn.run("api.routes:app", host="127.0.0.1", port=8001, log_level="info", workers=4)
+    uvicorn.run("run:app", host="127.0.0.1", port=8001, log_level="info", workers=4)
