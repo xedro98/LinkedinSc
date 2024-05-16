3db3a414c40690782d6809ef1b03d7da0e6751cd
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-05-16 11:33:29 +0530

    fg

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-import uvicorn
 from api.routes import app
+import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8001, log_level="info")
+    uvicorn.run(app, host="127.0.0.1", port=8001, log_level="info", workers=4)
