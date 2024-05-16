0a819a7e1c55ff40960487da6638ede04f35cf3c
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-05-16 11:35:08 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-from api.routes import app
 import uvicorn
+from fastapi import FastAPI
+from api.routes import router
+
+app = FastAPI()
+app.include_router(router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8001, log_level="info", workers=4)
+    uvicorn.run("run:app", host="127.0.0.1", port=8001, log_level="info", workers=4, reload=True)
