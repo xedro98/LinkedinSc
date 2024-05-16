8b606cd0df991423413f9efe048c33134fb6ebc3
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-05-16 17:15:51 +0530

    Update candidate_scraper.py

--- services/candidate_scraper.py
+++ services/candidate_scraper.py
@@ -1,11 +1,24 @@
-import time
-import random
-from fake_useragent import UserAgent
 from selenium import webdriver
+from time import sleep
 from services.scraping_utils import options, service, search_for_candidate_name, search_for_candidate_headline, search_for_section, add_session_cookie
 from concurrent.futures import ThreadPoolExecutor
 import json
 from multiprocessing import Pool
+import random
+from fake_useragent import UserAgent
+import time
+
+def scrape_linkedin_profile(linkedin_ids):
+    """Scraping LinkedIn profile data for multiple profiles"""
+    try:
+        with Pool(processes=len(linkedin_ids)) as pool:
+            results = pool.map(scrape_profile_worker, linkedin_ids)
+        
+        return results
+    except Exception as e:
+        print(f"Error fetching details for profiles: {e}")
+        return {"error": f"Error fetching profile details"}
+    
 
 def simulate_human_scroll(driver):
     # Get the current scroll position
@@ -40,28 +53,26 @@ def random_delay():
 def scrape_profile_worker(linkedin_id):
     try:
         # Setup Selenium WebDriver
-        ua = UserAgent()
-        user_agent = ua.random
-        options.add_argument(f'user-agent={user_agent}')
         driver = webdriver.Chrome(service=service, options=options)
-
+        
         # Load cookies from the file
         add_session_cookie(driver)
+        
         print(f'Scraping data for id: {linkedin_id}')
-
+        
         # LinkedIn URL for the profile
         profile_url = f"https://www.linkedin.com/in/{linkedin_id}/"
-
+        
         # Navigate to the LinkedIn profile
         driver.get(profile_url)
-
+        
         if "/404" in driver.current_url or "Page not found" in driver.page_source:
             driver.quit()
             print(f"Profile for {linkedin_id} not found (404)")
             return {"error": f"Profile for {linkedin_id} not found."}
-
-        random_delay()  # Add random delay
-
+        
+        sleep(1)
+        
         # Scrape name, experiences, education from the LinkedIn profile
         try:
             name = search_for_candidate_name(driver)
@@ -69,19 +80,17 @@ def scrape_profile_worker(linkedin_id):
                 driver.quit()
                 print("scraping failed due to session token not setup or expired")
                 return {"error": "Your Linkedin session token is not set up correctly or has expired"}
-
-            simulate_human_scroll(driver)  # Simulate human scrolling and mouse movements
-            random_delay()  # Add random delay
-
+            
             headline = search_for_candidate_headline(driver)
             education = search_for_section(driver, "Education")
             experience = search_for_section(driver, "Experience")
         except Exception as e:
             print(f"Error scraping details for {linkedin_id} : {e}")
             return {"error": f"Error searching for details for {linkedin_id}"}
-
+        
         driver.quit()
         print(f"finished fetching details for profile {linkedin_id} successfully")
+        
         return {
             "linkedin_id": linkedin_id,
             "name": name,
