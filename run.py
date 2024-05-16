8ad923a11052952938601b5e47865a0cda5b72d8
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-05-16 13:14:40 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import uvicorn
 from api.routes import app
 
 if __name__ == '__main__':
-    uvicorn.run("api.routes:app", host="127.0.0.1", port=8001, log_level="info", workers=8)
+    uvicorn.run("api.routes:app", host="127.0.0.1", port=8001, log_level="info", workers=2)
